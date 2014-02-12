e0bf2e3885c5322a1d37dfb6ce0f0a1658e8d619
何鑫 <58562503@qq.com>
2014-02-12 10:04:40 +0800

    添加Test类

---
+++ eos-server-example/src/main/java/com/sunsharing/component/test/TestServiceImpl.java
@@ -0,0 +1,42 @@
+package com.sunsharing.component.test;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by criss on 14-2-11.
+ */
+@Service
+public class TestServiceImpl implements TestType {
+    @Override
+    public int testInt(int i) {
+        return 0;
+    }
+
+    @Override
+    public double testDouble(double d) {
+        return 0;
+    }
+
+    @Override
+    public float testFloat(float f) {
+        return 0;
+    }
+
+    @Override
+    public String testString(String s, String sw) {
+        return "我是正真的服务方";
+    }
+
+    @Override
+    public Map testMap(Map m, String l2) {
+        return null;
+    }
+
+    @Override
+    public List testListMap(List list) {
+        return null;
+    }
+}

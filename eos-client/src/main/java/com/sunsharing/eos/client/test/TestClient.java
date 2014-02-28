0ccff9fdf78e175537cdcf114a2aacd768d3dc43
何鑫 <58562503@qq.com>
2014-02-28 12:53:57 +0800

    1.1.5发包

---
+++ eos-client/src/main/java/com/sunsharing/eos/client/test/TestClient.java
@@ -0,0 +1,15 @@
+package com.sunsharing.eos.client.test;
+
+import com.sunsharing.eos.client.EosInit;
+
+/**
+ * Created by criss on 14-2-27.
+ */
+public class TestClient {
+
+    public static void main(String[]a)
+    {
+        EosInit.synStart("com.sunsharing");
+    }
+
+}

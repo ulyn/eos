5ebab58d3af258987a1983e47db7ee18d80aca8b
ulyn <407620185@qq.com>
2014-09-07 15:33:11 +0800

    fastjson增加反序列化Object[].class的方式

--- eos-common/src/main/java/com/sunsharing/eos/common/serialize/support/json/FastJsonSerialization.java
+++ eos-common/src/main/java/com/sunsharing/eos/common/serialize/support/json/FastJsonSerialization.java
@@ -1,6 +1,13 @@
 package com.sunsharing.eos.common.serialize.support.json;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.sunsharing.eos.common.rpc.impl.RpcInvocation;
+import com.sunsharing.eos.common.rpc.protocol.HeartPro;
 import com.sunsharing.eos.common.serialize.ObjectInput;
 import com.sunsharing.eos.common.serialize.ObjectOutput;
 import com.sunsharing.eos.common.serialize.Serialization;
@@ -8,9 +15,15 @@ import com.sunsharing.eos.common.serialize.Serialization;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FastJsonSerialization implements Serialization {
+    static {
+        ParserConfig.getGlobalInstance().putDeserializer(Object[].class,
+                new ObjectArrayDeserializer());
+    }
 
     public byte getContentTypeId() {
         return 6;
@@ -28,4 +41,13 @@ public class FastJsonSerialization implements Serialization {
         return new FastJsonObjectInput(input);
     }
 
+    public static void main(String[] args) {
+        RpcInvocation rpc = new RpcInvocation();
+        Map map = new HashMap();
+        map.put("a", 1);
+        rpc.setArguments(new Object[]{map, 1, "1", 1.1, new HeartPro()});
+        String str = JSONObject.toJSONString(rpc);
+        System.out.println(str);
+        RpcInvocation de = JSON.parseObject(str, RpcInvocation.class);
+    }
 }
---
+++ eos-common/src/main/java/com/sunsharing/eos/common/serialize/support/json/ObjectArrayDeserializer.java
@@ -0,0 +1,52 @@
+/**
+ * @(#)RpcInvocationDeserializer
+ * 版权声明 厦门畅享信息技术有限公司, 版权所有 违者必究
+ *
+ *<br> Copyright:  Copyright (c) 2014
+ *<br> Company:厦门畅享信息技术有限公司
+ *<br> @author ulyn
+ *<br> 14-9-7 下午2:27
+ *<br> @version 1.0
+ *————————————————————————————————
+ *修改记录
+ *    修改者：
+ *    修改时间：
+ *    修改原因：
+ *————————————————————————————————
+ */
+package com.sunsharing.eos.common.serialize.support.json;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.DefaultJSONParser;
+import com.alibaba.fastjson.parser.deserializer.ObjectDeserializer;
+import com.sunsharing.eos.common.rpc.impl.RpcInvocation;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <pre></pre>
+ * <br>----------------------------------------------------------------------
+ * <br> <b>功能描述:</b>
+ * <br>
+ * <br> 注意事项:
+ * <br>
+ * <br>
+ * <br>----------------------------------------------------------------------
+ * <br>
+ */
+public class ObjectArrayDeserializer implements ObjectDeserializer {
+    @Override
+    public <T> T deserialze(DefaultJSONParser parser, Type type, Object fieldName) {
+        List<Object> list = new ArrayList<Object>();
+        parser.parseArray(list);
+        return (T) list.toArray();
+    }
+
+    @Override
+    public int getFastMatchToken() {
+        return 0;
+    }
+}
+

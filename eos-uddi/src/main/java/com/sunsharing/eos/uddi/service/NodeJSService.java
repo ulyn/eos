fcffb16d6b37d9081167ff338abddc746de7b3f7
ulyn <407620185@qq.com>
2014-09-15 15:42:07 +0800

    增加uuid发布nodejs服务的校验

--- eos-uddi/src/main/java/com/sunsharing/eos/uddi/service/NodeJSService.java
+++ eos-uddi/src/main/java/com/sunsharing/eos/uddi/service/NodeJSService.java
@@ -25,6 +25,7 @@ import com.sunsharing.eos.uddi.dao.SimpleHibernateDao;
 import com.sunsharing.eos.uddi.model.*;
 import com.sunsharing.eos.uddi.sys.SysInit;
 import com.sunsharing.eos.uddi.sys.WindowsExec;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 import org.apache.tools.ant.taskdefs.Sleep;
 import org.hibernate.SessionFactory;
@@ -91,7 +92,7 @@ public class NodeJSService {
         return map;
     }
 
-    public Map publishCNPM(String appId, String v) throws Exception {
+    public Map publishCNPM(String appId, String oldVersion) throws Exception {
         TApp app = appDao.get(new Integer(appId));
         List<TService> servicesTemp = serviceDao.find("from TService where appId=?", new Integer(appId));
         List<TService> services = new ArrayList<TService>();
@@ -102,14 +103,21 @@ public class NodeJSService {
             }
         }
         if (services.size() > 0) {
-            if (StringUtils.isBlank(v)) {
-                v = "0.0.1";
-            } else {
-                String versionStr = v.split("\\.")[2];
+            List changeServices = new ArrayList();
+            String v = "0.0.1";
+            if (!StringUtils.isBlank(oldVersion)) {
+                String versionStr = oldVersion.split("\\.")[2];
                 int verUpdate = Integer.parseInt(versionStr) + 1;
                 v = "0.0." + verUpdate;
             }
-            String path = createDir(app, v, services);
+            //判断当前版本与上一版本的服务变化情况。
+            String oldVersionPath = SysInit.path + File.separator + "node_service_modules"
+                    + File.separator + app.getAppCode() + File.separator + oldVersion;
+            changeServices = getChangeServices(oldVersionPath, services);
+            if (changeServices.size() == 0) {
+                throw new RuntimeException("服务版本没有变化，不需要重复发布服务！");
+            }
+            String path = createDir(app, v, services, changeServices);
             String result = new WindowsExec().run("cnpm publish " + path);
             if (result.indexOf("ERR") != -1) {
                 throw new RuntimeException("发布服务失败：" + result);
@@ -124,7 +132,7 @@ public class NodeJSService {
         }
     }
 
-    private String createDir(TApp app, String v, List<TService> services) throws Exception {
+    private String createDir(TApp app, String v, List<TService> services, List changeServices) throws Exception {
         String path = SysInit.path + File.separator + "node_service_modules"
                 + File.separator + app.getAppCode() + File.separator + v;
         File dir = new File(path);
@@ -132,8 +140,9 @@ public class NodeJSService {
             dir.mkdirs();
         }
         createPackageJsonFile(app, v, path);
-        createReadme(app, v, path, services);
+        createReadme(app, v, path, services, changeServices);
         createJs(services, path);
+        createVersionCheckFile(path, services);
         return path;
     }
 
@@ -168,20 +177,80 @@ public class NodeJSService {
                 "  },\n" +
                 "  \"license\": \"ISC\"\n" +
                 "}\n";
-        FileUtil.createFile(path + File.separator + "package.json", content);
+        FileUtil.createFile(path + File.separator + "package.json", content, "utf-8");
     }
 
-    private void createReadme(TApp app, String v, String path, List<TService> services) throws Exception {
+    /**
+     * 保存服务的版本号
+     *
+     * @param path
+     * @param services
+     * @throws Exception
+     */
+    private void createVersionCheckFile(String path, List<TService> services) throws Exception {
+        JSONObject jo = new JSONObject();
+        for (TService service : services) {
+            jo.put(service.getServiceCode(), service.getVersions().get(0).getVersion());
+        }
+        FileUtil.createFile(path + File.separator + "services.version", JSON.toJSONString(jo, true), "utf-8");
+    }
+
+    private List getChangeServices(String path, List<TService> services) throws IOException {
+        JSONObject jo = new JSONObject();
+        try {
+            File file = new File(path + File.separator + "services.version");
+            if (file.exists()) {
+                InputStream inputStream = new FileInputStream(file);
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(inputStream, writer, "utf-8");
+                String txt = writer.toString();
+                if (!StringUtils.isBlank(txt)) {
+                    jo = JSON.parseObject(txt);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("取得版本文件异常", e);
+        }
+        List list = new ArrayList();
+        for (TService service : services) {
+            String oldV = jo.getString(service.getServiceCode());
+            String newV = service.getVersions().get(0).getVersion();
+            Map map = new HashMap();
+            if (StringUtils.isBlank(oldV)) {
+                map.put("name", service.getServiceCode());
+                map.put("oldV", "未发布");
+                map.put("newV", newV);
+                list.add(map);
+            } else if (!oldV.equals(newV)) {
+                map.put("name", service.getServiceCode());
+                map.put("oldV", oldV);
+                map.put("newV", newV);
+                list.add(map);
+            }
+        }
+        return list;
+    }
+
+    private void createReadme(TApp app, String v, String path, List<TService> services, List changeServices) throws Exception {
         StringBuilder sb = new StringBuilder(app.getAppName() + "-" + app.getAppCode() + "\n" +
                 "========\n" +
                 "\n" +
-                app.getAppName() + "-" + app.getAppCode() + " 的eos服务\n" +
-                "\n" +
-                "## 服务接口\n");
-        for (TService service : services) {
-            sb.append("+ [" + service.getServiceCode() + " " + service.getServiceName() + "](#)\n");
+                app.getAppName() + "-" + app.getAppCode() + " 的eos服务,详见http://192.168.0.169:8100/index.html#/servicelist/" + app.getAppId() + "/0 \n" +
+                "\n");
+        sb.append("## 服务更新情况 \n");
+        for (int i = 0; i < changeServices.size(); i++) {
+            sb.append(i + ". ");
+            Map map = (Map) changeServices.get(i);
+            sb.append(map.get("name"));
+            sb.append("  ");
+            sb.append(map.get("oldV") + "->" + map.get("newV"));
+            sb.append("\n");
         }
-        FileUtil.createFile(path + File.separator + "README.md", sb.toString());
+        sb.append("\n## 服务接口\n");
+        for (TService service : services) {
+            sb.append("+ " + service.getServiceCode() + "-" + service.getVersions().get(0).getVersion() + "-" + service.getServiceName() + "\n");
+        }
+        FileUtil.createFile(path + File.separator + "README.md", sb.toString(), "utf-8");
     }
 
     private void createJs(List<TService> services, String path) throws Exception {
@@ -208,7 +277,7 @@ public class NodeJSService {
         }
         sb.append("    }\n" +
                 "}");
-        FileUtil.createFile(path + File.separator + "index.js", sb.toString());
+        FileUtil.createFile(path + File.separator + "index.js", sb.toString(), "utf-8");
     }
 
     private void createServiceJs(TService service, String path) throws Exception {
@@ -258,7 +327,7 @@ public class NodeJSService {
         }
         sb.append("    return " + service.getServiceCode() + ";\n" +
                 "}");
-        FileUtil.createFile(file, sb.toString());
+        FileUtil.createFile(file, sb.toString(), "utf-8");
     }
 }
 
